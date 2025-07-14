c8c0d2946de874db82741fcbc279a334f9b4bd5e
Parallaxes <lucasmasterson@gmail.com>
2025-07-14 16:56:01 -0700

    Some updates

--- src/challenge06/challenge06.rs
+++ src/challenge06/challenge06.rs
@@ -1,6 +1,9 @@
-use super::utils::{IAmCamera, Message, Plate, parse};
+use crate::challenge06::db::{assign_id};
+
+use super::utils::{IAmCamera, Message, Plate, parse, Client, ClientType};
 use std::collections::{BTreeMap, HashSet};
 use std::error::Error;
+use std::net::SocketAddr;
 use std::sync::Arc;
 use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader};
 use tokio::net::{TcpListener, TcpStream};
@@ -10,22 +13,26 @@ pub async fn run() -> Result<(), Box<dyn Error>> {
     let listener = TcpListener::bind("0.0.0.0:8000").await?;
     println!("Server opened on port 8000");
 
-    let cameras_db = Arc::new(Mutex::new(BTreeMap::<IAmCamera, u32>::new()));
-    let plate_db = Arc::new(Mutex::new(BTreeMap::<Plate, u32>::new()));
-    let id_db = Arc::new(Mutex::new(BTreeMap::<TcpStream, u32>::new()));
+    // let cameras_db = Arc::new(Mutex::new(BTreeMap::<IAmCamera, u32>::new()));
+    // let plate_db = Arc::new(Mutex::new(BTreeMap::<Plate, u32>::new()));
+    // let id_db = Arc::new(Mutex::new(BTreeMap::<TcpStream, u32>::new()));
+    let id_db = Arc::new(Mutex::new(BTreeMap::<SocketAddr, Client>::new()));
 
     loop {
         let (client, addr) = listener.accept().await?;
         println!("New connection from {}", addr);
+        let id_db_clone = id_db.clone();
         tokio::spawn(async move {
-            if let Err(e) = handle_connection(client).await {
+            if let Err(e) = handle_connection(client, id_db_clone).await {
                 eprintln!("Connection error: {:?}", e);
             }
         });
     }
 }
 
-async fn handle_connection(client: TcpStream) -> Result<(), Box<dyn Error>> {
+async fn handle_connection(client: TcpStream, id_db: Arc<Mutex<BTreeMap<std::net::SocketAddr, Client>>>) -> Result<(), Box<dyn Error>> {
+    let peer_addr = client.peer_addr()?;
+    
     let (reader, mut writer) = client.into_split();
     let mut client_buf = BufReader::new(reader);
     let mut buf = [0u8; 1024];
@@ -37,8 +44,17 @@ async fn handle_connection(client: TcpStream) -> Result<(), Box<dyn Error>> {
         }
 
         if let Some(msg) = parse(&buf) {
+            let id_db_clone = id_db.clone();
             match msg {
-                Message::IAmCamera(_) => panic!(),
+                Message::IAmCamera(_) => {
+                    assign_id(peer_addr, id_db_clone, ClientType::Camera).await.expect("Failed to assign ID");
+                },
+                Message::IAmDispatcher(_) => {
+                    assign_id(peer_addr, id_db_clone, ClientType::Dispatcher).await.expect("Failed to assign ID");
+                },
+                Message::Plate(_) => {
+                    // TODO
+                }
                 _ => panic!(),
             }
         }
--- src/challenge06/db.rs
+++ src/challenge06/db.rs
@@ -1,33 +1,34 @@
 use std::collections::{BTreeMap, HashSet};
-use std::net::{SocketAddr, TcpStream};
+use tokio::net::TcpStream;
+use std::net::SocketAddr;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
 use crate::challenge06::utils::{Client, ClientType, IAmCamera, IAmDispatcher};
 
-// TODO: Write tests + refactor
-pub async fn assign_id<T>(
-    stream: TcpStream,
+#[derive(Debug)]
+pub enum AssignError {
+    InvalidClientType,
+}
+
+pub async fn assign_id(
+    peer_addr: SocketAddr,
     id_db: Arc<Mutex<BTreeMap<SocketAddr, Client>>>,
-    client: T,
-) -> Option<()> {
+    client_type: ClientType,
+) -> Result<(), AssignError> {
     let mut db_guard = id_db.lock().await;
-    let new_id = db_guard
-        .values()
+    let new_id = generate_next_id(&db_guard);
+    let new_client = Client::new(new_id, client_type);
+
+    db_guard.insert(peer_addr, new_client);
+    Ok(())
+}
+
+fn generate_next_id(db: &BTreeMap<SocketAddr, Client>) -> u32 {
+    db.values()
         .map(|client| client.get_id())
         .max()
         .map(|max_id| max_id + 1)
-        .unwrap_or(0);
-    let new_client = match client {
-        IAmCamera => Client::new(new_id, ClientType::Camera),
-        IAmDispatcher => Client::new(new_id, ClientType::Dispatcher),
-        _ => return None,
-    };
-    //let new_id = db_guard.values().max().map(|i| i + 1).ok_or("Failed to generate ID").unwrap();
-    if let Some(peer_addr) = stream.peer_addr().ok() {
-        db_guard.insert(peer_addr, new_client);
-        return Some(());
-    }
-
-    None
+        .unwrap_or(0)
 }
+
